5feff0909e8ea90b588a0f546b48eaa7e4640f99
Minsu Kim <qmdlrhdfyd@naver.com>
2022-11-14 00:14:52 +0900

    implement basic player move

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+
+    }
+}
