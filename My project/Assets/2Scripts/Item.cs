0bafe8d99709ecb5ff96cde8f9b6fc86df3e244d
Minsu Kim <qmdlrhdfyd@naver.com>
2022-11-15 17:59:02 +0900

    make Items

---
+++ My project/Assets/2Scripts/Item.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public enum Type { Ammo, Coin, Granade, Heart, Weapon };
+    public Type type;
+    public int value;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+
+}
