e7da7e9f098ff8de4ccc4fae76ea65cad4d68ddc
Minsu Kim <qmdlrhdfyd@naver.com>
2022-11-20 16:21:01 +0900

    implement range attack

--- My project/Assets/2Scripts/Player.cs
+++ My project/Assets/2Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public int ammo;
     public int coin;
     public int health;
+    public Camera followCamera;
     
     public int maxAmmo;
     public int maxCoin;
@@ -26,6 +27,7 @@ public class Player : MonoBehaviour
     bool wDown;
     bool jDown;
     bool fDown;
+    bool rDown;
     bool iDown;
     bool sDown1;
     bool sDown2;
@@ -34,6 +36,7 @@ public class Player : MonoBehaviour
     bool isJump;
     bool isDodge;
     bool isSwap;
+    bool isReload;
     bool isFireReady = true;
 
     Vector3 moveVec;
@@ -60,6 +63,7 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Attack();
+        Reload();
         Dodge();
         Swap();
         Interation();
@@ -71,7 +75,8 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
-        fDown = Input.GetButtonDown("Fire1");
+        fDown = Input.GetButton("Fire1");
+        rDown = Input.GetButtonDown("Reload");
         iDown = Input.GetButtonDown("Interation");
         sDown1 = Input.GetButtonDown("Swap1");
         sDown2 = Input.GetButtonDown("Swap2");
@@ -86,7 +91,7 @@ public class Player : MonoBehaviour
             moveVec = dodgeVec;
         }
 
-        if(isSwap || !isFireReady){
+        if(isSwap || isReload || !isFireReady){
             moveVec = Vector3.zero;
         }
 
@@ -98,7 +103,20 @@ public class Player : MonoBehaviour
 
     void Turn()
     {
+        // #1. 키보드에 의한 회전
         transform.LookAt(transform.position + moveVec);
+
+        // #2. 마우스에 의한 회전
+        if(fDown){
+            Ray ray = followCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100)){
+                Vector3 nextVec = rayHit.point - transform.position;
+                nextVec.y = 0;
+                transform.LookAt(transform.position + nextVec);
+            }
+        }
+
     }
 
     void Jump()
@@ -122,9 +140,35 @@ public class Player : MonoBehaviour
 
         if (fDown && isFireReady && !isDodge && !isSwap){
             equipWeapon.Use();
-            anim.SetTrigger("doSwing");
+            anim.SetTrigger(equipWeapon.type == Weapon.Type.Melee ? "doSwing" : "doShot");
             fireDelay = 0;
         }
+    }
+
+    void Reload()
+    {
+        if (equipWeapon == null)
+            return;
+
+        if (equipWeapon.type == Weapon.Type.Melee)
+            return;
+
+        if (ammo == 0)
+            return;
+
+        if (rDown && !isJump && !isDodge && !isSwap && isFireReady){
+            anim.SetTrigger("doReload");
+            isReload = true;
+            Invoke("ReloadOut", 3f);
+
+        }
+    }
+
+    void ReloadOut(){
+        int reAmmo = ammo < equipWeapon.maxAmmo ? ammo : equipWeapon.maxAmmo; 
+        equipWeapon.curAmmo = reAmmo;
+        ammo -= reAmmo; 
+        isReload = false;
     }
 
     void Dodge()
--- My project/Assets/2Scripts/Weapon.cs
+++ My project/Assets/2Scripts/Weapon.cs
@@ -8,14 +8,26 @@ public class Weapon : MonoBehaviour
     public Type type;
     public int damage;
     public float rate;
+    public int maxAmmo;
+    public int curAmmo;
+
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
 
     public void Use()
     {
         if (type == Type.Melee){
             StopCoroutine("Swing");
             StartCoroutine("Swing");
+        }
+        else if (type == Type.Range && curAmmo > 0){
+            curAmmo--;
+            StartCoroutine("Shot");
+
         }
     }
 
@@ -34,5 +46,21 @@ public class Weapon : MonoBehaviour
 
     }
     
+
+    IEnumerator Shot()
+    {
+        // #1. 총알발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+
+        yield return null;
+        // #2. 탄피배출
+        GameObject instantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = instantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec, ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10, ForceMode.Impulse);
+    }
 
 }
